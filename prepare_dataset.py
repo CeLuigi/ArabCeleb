b047fb9c95c39e6b8489fa56afa4e09dbfdbd055
Flavio Piccoli <flavio.piccoli@unimib.it>
2021-10-05 17:46:14 +0200

    Handling exceptions of missing videos.

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,6 +1,7 @@
 import os
 import json
 import datetime
+import pytube
 from pytube import YouTube
 
 
@@ -9,8 +10,10 @@ if __name__ == '__main__':
 	if not os.path.exists(dest_dir):
 		os.makedirs(dest_dir)
 
-	with open("info.json") as f:
+	with open("utterance_info.json") as f:
 		data = json.load(f)
+
+	not_available = []
 
 	for key,value in data.items():
 		celeb_datadir = os.path.join(dest_dir, key)
@@ -19,8 +22,12 @@ if __name__ == '__main__':
 
 		for id_video, (url, value) in enumerate(value['utterances'].items()):
 			yt = YouTube(url)
-			s = yt.streams.filter(only_audio=True)[0]
-			filename = str(id_video)
+			try:
+				s = yt.streams.filter(only_audio=True)[0]
+			except pytube.exceptions.VideoPrivate as e:
+				not_available.append(url)
+				continue
+			filename = str(id_video) + '.mp4'
 			s.download('.', filename=filename)
 
 			for utterance in value:
@@ -29,9 +36,13 @@ if __name__ == '__main__':
 				utterance_to = utterance['to']
 				wav_filename = os.path.join(celeb_datadir, utterance_fn)
 				os.system('ffmpeg -y -i {} -acodec pcm_s16le '
-				 '-ss {} -to {} {}'.format(filename+'.mp4',
+				 '-ss {} -to {} {}'.format(filename,
 				 	str(datetime.timedelta(seconds=utterance_from)),
 				 	str(datetime.timedelta(seconds=utterance_to)),
 				 	wav_filename))
-
-			os.remove(filename+'.mp4')
+			os.remove(filename)
+			
+	print('---------------------------------------------')
+	print('The following videos are no longer available:')
+	for na in not_available: print(na)
+	print('---------------------------------------------')
